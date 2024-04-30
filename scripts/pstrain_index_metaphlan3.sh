1a391af61efff56b1bed301c5fc7cd6defb84c94
wshuai294 <wshuai294@gmail.com>
2024-04-30 15:01:38 +0800

    db dir

--- scripts/pstrain_index_metaphlan3.sh
+++ scripts/pstrain_index_metaphlan3.sh
@@ -7,10 +7,10 @@ mkdir $script_dir/../mpa_v31_CHOCOPhlAn_201901
 cd $script_dir/../mpa_v31_CHOCOPhlAn_201901
 
 wget http://cmprod1.cibio.unitn.it/biobakery3/metaphlan_databases/mpa_v31_CHOCOPhlAn_201901.tar
-tar -xvf script_dir/../mpa_v31_CHOCOPhlAn_201901.tar
+tar -xvf $script_dir/../mpa_v31_CHOCOPhlAn_201901.tar
 
-bzip2 -d script_dir/../mpa_v31_CHOCOPhlAn_201901.fna.bz2
-bowtie2-build script_dir/../mpa_v31_CHOCOPhlAn_201901.fna script_dir/../mpa_v31_CHOCOPhlAn_201901
+bzip2 -d $script_dir/../mpa_v31_CHOCOPhlAn_201901.fna.bz2
+bowtie2-build $script_dir/../mpa_v31_CHOCOPhlAn_201901.fna $script_dir/../mpa_v31_CHOCOPhlAn_201901
 
 
 
--- scripts/pstrain_index_metaphlan4.sh
+++ scripts/pstrain_index_metaphlan4.sh
@@ -8,10 +8,10 @@ mkdir $script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403
 cd $script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403
 
 wget http://cmprod1.cibio.unitn.it/biobakery4/metaphlan_databases/mpa_vJun23_CHOCOPhlAnSGB_202403.tar
-tar -xvf script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403.tar
+tar -xvf $script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403.tar
 
-bzip2 -d script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403.fna.bz2
-bowtie2-build script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403.fna script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403
+bzip2 -d $script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403.fna.bz2
+bowtie2-build $script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403.fna $script_dir/../mpa_vJun23_CHOCOPhlAnSGB_202403
 
 
 
