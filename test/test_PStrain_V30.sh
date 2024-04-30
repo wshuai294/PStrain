50cb73def45a6f2ca59dc45fee25d7c1c999e543
wshuai294 <wshuai294@gmail.com>
2024-04-30 14:36:59 +0800

    test code

--- test/test_PStrain_V30.sh
+++ test/test_PStrain_V30.sh
@@ -1,1 +1,1 @@
-python3 ../scripts/PStrain.py -c config.txt -o out --bowtie2db ../mpa_v31_CHOCOPhlAn_201901/ -x mpa_v31_CHOCOPhlAn_201901
+python3 ../scripts/PStrain.py --metaphlan_version 3  -c config.txt -o out --bowtie2db ../mpa_v31_CHOCOPhlAn_201901/ -x mpa_v31_CHOCOPhlAn_201901
